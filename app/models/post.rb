40c64a07c1229a5aeb36e98886037aa23210a2ac
Bruno Gonçales <bgl.bruno@gmail.com>
2015-06-11 21:03:27 -0300

    Adiciona relações e validações

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :title, type: String
   field :content, type: String
+  
+  has_many :comments
+
+  validates_presence_of :title, :content
+  validates_length_of :content, minimum: 15
 end
